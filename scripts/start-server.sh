db15609523939a5285d26ac45f1d820f19c8df9d
ich777 <ich777@noreply.example.org>
2020-10-11 05:52:09 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,48 +1,6 @@
 #!/bin/bash
 export DISPLAY=:99
-CUR_V="$(${DATA_DIR}/thunderbird --version 2>/dev/null | cut -d ' ' -f3)"
-if [ "${THUNDERBIRD_V}" == "latest" ]; then
-	LAT_V="$(wget -qO- https://github.com/ich777/versions/raw/master/Thunderbird | grep LATEST | cut -d '=' -f2)"
-	sleep 2
-	THUNDERBIRD_V="${LAT_V}"
-	if [ -z "$LAT_V" ]; then
-		if [ ! -z "$CUR_V" ]; then
-			echo "---Can't get latest version of Thunderbird falling back to v$CUR_V---"
-			LAT_V="$CUR_V"
-		else
-			echo "---Something went wrong, can't get latest version of Thunderbird, putting container into sleep mode---"
-			sleep infinity
-		fi
-	fi
-fi
 
-echo "---Version Check---"
-if [ -z "$CUR_V" ]; then
-	echo "---Thunderbird not installed, installing---"
-	cd ${DATA_DIR}
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/Thunderbird-$LAT_V-$THUNDERBIRD_LANG.tar.bz2 "https://archive.mozilla.org/pub/thunderbird/releases/${THUNDERBIRD_V}/linux-x86_64/${THUNDERBIRD_LANG}/thunderbird-${THUNDERBIRD_V}.tar.bz2" ; then
-		echo "---Sucessfully downloaded Thunderbird---"
-	else
-		echo "---Something went wrong, can't download Thunderbird, putting container in sleep mode---"
-		sleep infinity
-	fi
-	tar -C ${DATA_DIR} --strip-components=1 -xf ${DATA_DIR}/Thunderbird-$LAT_V-$THUNDERBIRD_LANG.tar.bz2
-	rm -R ${DATA_DIR}/Thunderbird-$LAT_V-$THUNDERBIRD_LANG.tar.bz2
-elif [ "$CUR_V" != "$LAT_V" ]; then
-	echo "---Version missmatch, installed v$CUR_V, downloading and installing latest v$LAT_V...---"
-    cd ${DATA_DIR}
-	find . -maxdepth 1 ! -name profile -exec rm -rf {} \; 2>/dev/null
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/Thunderbird-$LAT_V-$THUNDERBIRD_LANG.tar.bz2 "https://archive.mozilla.org/pub/thunderbird/releases/${THUNDERBIRD_V}/linux-x86_64/${THUNDERBIRD_LANG}/thunderbird-${THUNDERBIRD_V}.tar.bz2" ; then
-		echo "---Sucessfully downloaded Thunderbird---"
-	else
-		echo "---Something went wrong, can't download Thunderbird, putting container in sleep mode---"
-		sleep infinity
-	fi
-	tar -C ${DATA_DIR} --strip-components=1 -xf ${DATA_DIR}/Thunderbird-$LAT_V-$THUNDERBIRD_LANG.tar.bz2
-	rm -R ${DATA_DIR}/Thunderbird-$LAT_V-$THUNDERBIRD_LANG.tar.bz2
-elif [ "$CUR_V" == "$LAT_V" ]; then
-	echo "---Thunderbird v$CUR_V up-to-date---"
-fi
 
 echo "---Preparing Server---"
 if [ ! -d ${DATA_DIR}/profile ]; then
@@ -88,4 +46,4 @@ sleep 2
 
 echo "---Starting Thunderbird---"
 cd ${DATA_DIR}
-${DATA_DIR}/thunderbird --display=:99 --profile ${DATA_DIR}/profile --P ${USER} --setDefaultMail ${EXTRA_PARAMETERS}
+/usr/bin/thunderbird --display=:99 --profile ${DATA_DIR}/profile -P ${USER} --setDefaultMail ${EXTRA_PARAMETERS}
