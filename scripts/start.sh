19cf4782d2540bca1ea28b961e064d7f722eb7c6
ich777 <ich777@noreply.example.org>
2020-10-11 05:05:57 +0200

    „scripts/start.sh“ hinzufügen

---
+++ scripts/start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#!/bin/bash
+echo "---Checking if UID: ${UID} matches user---"
+usermod -u ${UID} ${USER}
+echo "---Checking if GID: ${GID} matches user---"
+usermod -g ${GID} ${USER}
+echo "---Setting umask to ${UMASK}---"
+umask ${UMASK}
+
+echo "---Checking for optional scripts---"
+if [ -f /opt/scripts/user.sh ]; then
+	echo "---Found optional script, executing---"
+    chmod +x /opt/scripts/user.sh
+    /opt/scripts/user.sh
+else
+	echo "---No optional script found, continuing---"
+fi
+
+echo "---Starting...---"
+chown -R ${UID}:${GID} /opt/scripts
+chown -R ${UID}:${GID} ${DATA_DIR}
+
+term_handler() {
+	kill -SIGTERM "$killpid"
+	wait "$killpid" -f 2>/dev/null
+	exit 143;
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+su ${USER} -c "/opt/scripts/start-server.sh" &
+killpid="$!"
+while true
+do
+	wait $killpid
+	exit 0;
+done
