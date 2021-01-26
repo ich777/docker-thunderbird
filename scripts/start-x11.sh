3c48930606a9caaeec1eeae38f8e06beaad54ce6
ich777 <ich777@noreply.example.org>
2021-01-26 14:50:25 +0100

    „scripts/start-x11.sh“ ändern

--- scripts/start-x11.sh
+++ scripts/start-x11.sh
@@ -1,4 +1,4 @@
-until x11vnc -display :99 -rfbport 5900 -shared -forever; do
+until x11vnc -display :99 -rfbport ${RFB_PORT} -shared -forever ${X11VNC_PARAMS}; do
     echo "x11vnc server crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
