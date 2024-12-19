4f65abca39a8bc0e8bfff2010d771763e606a3a2
Christoph Hummer <christophhummer@gmail.com>
2024-12-19 15:20:34 +0100

    scripts/start.sh aktualisiert

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,8 +29,9 @@ chown -R ${UID}:${GID} ${DATA_DIR}
 
 echo "---Starting...---"
 term_handler() {
-	kill -SIGTERM "$killpid"
-	wait "$killpid" -f 2>/dev/null
+	su ${USER} -c 'export ${DISPLAY:-:99}; wmctrl -l | grep "Thunderbird" | awk "{print \$1}" | xargs -I {} wmctrl -ic {}'
+	sleep 2
+	tail --pid=$(pidof thunderbird) -f 2>/dev/null
 	exit 143;
 }
 
